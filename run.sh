578f22303a95ae9216fe428c5de1eae07dd57a51
harish-myaccount <harish.myemails@gmail.com>
2016-11-08 18:17:48 +0530

    Update run.sh (#1)

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker stop cas &>/dev/null
 docker rm cas -f &>/dev/null
 
-docker run -p 80:8080 -p 443:8443 -d --name="cas" apereo/cas:v4.1.5
+docker run -p 80:8080 -p 443:8443 -d --name="cas" apereo/cas:v$1
