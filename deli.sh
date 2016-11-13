8a2812a1f5e300afabebbdfad41fdbb5d1a462bf
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-13 13:26:52 -0700

    replaced oracle java

--- deli.sh
+++ deli.sh
@@ -2,6 +2,6 @@
 
 clear
 docker rmi -f $(docker images -a | grep "^<none>" | awk '{print $3}')
+docker ps -a -q -f status=exited | xargs docker rm -v
+docker images -f "dangling=true" -q | xargs docker rmi -f
 docker images
-# docker rm -v $(docker ps -a -q -f status=exited)
-# docker rmi $(docker images -f "dangling=true" -q)
