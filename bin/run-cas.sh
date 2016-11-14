bfb6dc6ac4f6cc94d4a5ab1b2b3b7e0e3e2018af
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-13 17:10:26 -0700

    working on image for cas 5

--- bin/run-cas.sh
+++ bin/run-cas.sh
@@ -4,5 +4,4 @@ export PATH=$PATH:$JAVA_HOME/bin:.
 # echo "Use of this image/container constitutes acceptence of the Oracle Binary Code License Agreement for Java SE."
 cd /cas-overlay/
 echo -e "Executing build from directory:" && pwd
-exec ./build.sh package -U
 exec ./build.sh run
--- run.sh
+++ run.sh
@@ -2,4 +2,17 @@
 docker stop cas &>/dev/null
 docker rm cas -f &>/dev/null
 
-docker run -p 8080:8080 -p 8443:8443 --name="cas" apereo/cas:v$1
+cas_version=$1
+
+if [ $# -eq 0 ] || [ -z "$cas_version" ]
+  then
+    echo "No CAS version is specified."
+    read -p "Provide a tag for the CAS image you are about to run (i.e. 5.0.0): " cas_version;
+fi
+
+if [ ! -z "$cas_version" ]
+  then
+	docker run -p 8080:8080 -p 8443:8443 --name="cas" apereo/cas:v$cas_version
+  else
+  	echo "No image tag is provided."	
+fi
