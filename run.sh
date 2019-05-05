c41fb6d29638e2ad57210dea8241853e098b785a
Misagh Moayyed <mmoayyed@unicon.net>
2019-05-05 10:45:16 -0700

    fixes

--- run.sh
+++ run.sh
@@ -10,10 +10,16 @@ if [ $# -eq 0 ] || [ -z "$cas_version" ]
     read -p "Provide a tag for the CAS image you are about to run (i.e. 5.0.0): " cas_version;
 fi
 
+image_tag="v${cas_version}"
+if [[ "${cas_version}" == *"master"* ]]; then
+  image_tag="latest"
+fi
+
 if [ ! -z "$cas_version" ]
   then
-	docker run -d -p 8080:8080 -p 8443:8443 --name="cas" apereo/cas:v$cas_version
+	docker run -d -p 8080:8080 -p 8443:8443 --name="cas" apereo/cas:"${image_tag}"
 	docker logs -f cas
   else
-  	echo "No image tag is provided."	
+  	echo "No image tag is provided."
+    exit 1
 fi
